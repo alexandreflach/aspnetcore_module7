bebdf8f8ff7382f01ae640c58c5b6b8f0d6fdca3
alexandreflach <contato@alexandreflach.com.br>
2018-04-04 20:33:34 -0300

    Add Migrations

--- src/StoreBuild.Web/Startup.cs
+++ src/StoreBuild.Web/Startup.cs
@@ -22,7 +22,7 @@ namespace StoreBuild.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Boostrap.Configure(services, configuration.GetConnectionString("DefaultConnection"));
+            Bootstrap.Configure(services, Configuration.GetSection("ConnectionString")["DefaultConnection"]);
             services.AddMvc();
         }
 
